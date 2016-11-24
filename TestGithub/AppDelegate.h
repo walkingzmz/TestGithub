668e13b58d1837862cf3c2da24023c5ab8425b96
walkingzmz <walking_zmz@163.com>
2016-11-24 14:38:31 +0800

    newscode

---
+++ TestGithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGithub
+//
+//  Created by CiHon-IOS2 on 2016/11/24.
+//  Copyright © 2016年 walkingzmz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGithub
+//
+//  Created by CiHon-IOS2 on 2016/11/24.
+//  Copyright © 2016年 walkingzmz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
